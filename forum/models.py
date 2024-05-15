802d0081d108fb9ac60c7f21dd48b4697d898a75
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2024-05-15 21:24:51 +0300

    new: models for forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Forum(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Branch(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    forum = models.ForeignKey(Forum, models.CASCADE, "branches")
