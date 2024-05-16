3d11e98749bf844588946700ffa003cc9ce85c25
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2024-05-16 19:42:01 +0300

    new: making migrations

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Poll(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name="questions")
+
+
+class Option(models.Model):
+    text = models.TextField()
+    answer = models.BooleanField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="options")
