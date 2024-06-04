48dcdfaa495bb01c6a05d5ed01d07534295e24d4
slurtty <slurtty@gmail.com>
2024-06-04 15:35:49 +0200

    add detail view, listview

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,33 +6,12 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    screenshots = models.ImageField(upload_to='')
-    links = models.URLField()
-    files = models.FileField(upload_to='')
+    screenshot = models.ImageField(upload_to='project_screenshots/', null = True, blank = True)
+    links = models.URLField(null = True, blank = True)
+    files = models.FileField(upload_to='project_files/', null = True, blank = True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
-    
-
-class CustomUser(AbstractUser):
-    portfolio_description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.username
-
-
-
-class Permission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    can_create_portfolio = models.BooleanField(default=False)
-    can_edit_portfolio = models.BooleanField(default=False)
-    can_delete_portfolio = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Permissions for {self.user.username}"
-
-
 
 
