14c3179095df00f86112514a888551da807dfd6a
Володимир <volodymyr.logika@gmail.com>
2024-06-06 20:37:23 +0300

    add Roles and UserProfile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+    permissions = models.ManyToManyField("auth.Permission")
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.user.username
